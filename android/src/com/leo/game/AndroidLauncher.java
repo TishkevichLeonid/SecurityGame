56a1486a5429a1de076fcbd43ca0a1aed28c671d
TishkevichLeonid <leonidtishkevich@gmail.com>
2017-02-07 14:27:26 +0300

    1st commit

---
+++ android/src/com/leo/game/AndroidLauncher.java
@@ -0,0 +1,16 @@
+package com.leo.game;
+
+import android.os.Bundle;
+
+import com.badlogic.gdx.backends.android.AndroidApplication;
+import com.badlogic.gdx.backends.android.AndroidApplicationConfiguration;
+import com.leo.game.Security;
+
+public class AndroidLauncher extends AndroidApplication {
+	@Override
+	protected void onCreate (Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		AndroidApplicationConfiguration config = new AndroidApplicationConfiguration();
+		initialize(new Security(), config);
+	}
+}
