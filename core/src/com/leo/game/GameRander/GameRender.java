4d0f9013f44f65bbf3ac4d7723d7c7a2691a6216
TishkevichLeonid <leonidtishkevich@gmail.com>
2017-02-08 16:55:22 +0300

    what to do

--- core/src/com/leo/game/GameRander/GameRender.java
+++ core/src/com/leo/game/GameRander/GameRender.java
@@ -34,7 +34,7 @@ public class GameRender {
 
     }
 
-    public void render(){
+    public void render(float runTime){
 
         Gdx.gl.glClearColor(0, 0, 0, 1);
         Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
