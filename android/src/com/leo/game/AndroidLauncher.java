ec692d39e340464a2712263594cec74c4646f3d3
TishkevichLeonid <leonidtishkevich@gmail.com>
2017-03-14 20:14:03 +0300

    make gpgs

--- android/src/com/leo/game/AndroidLauncher.java
+++ android/src/com/leo/game/AndroidLauncher.java
@@ -1,12 +1,21 @@
 package com.leo.game;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.WindowManager;
 
 import com.badlogic.gdx.backends.android.AndroidApplication;
 import com.badlogic.gdx.backends.android.AndroidApplicationConfiguration;
+import com.google.android.gms.games.Games;
+import com.google.android.gms.games.leaderboard.Leaderboards;
+import com.google.example.games.basegameutils.GameHelper;
 import com.leo.game.Security;
 
-public class AndroidLauncher extends AndroidApplication {
+public class AndroidLauncher extends AndroidApplication implements GameHelper.GameHelperListener, ActionResolver {
+
+	private GameHelper gameHelper;
+	private Security game;
+
 	@Override
 	protected void onCreate (Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
@@ -14,5 +23,102 @@ public class AndroidLauncher extends AndroidApplication {
 		config.useAccelerometer = false;
 		config.useCompass = false;
 		initialize(new Security(), config);
+
+		gameHelper = new GameHelper(this, GameHelper.CLIENT_ALL);
+		gameHelper.setConnectOnStart(false);
+		gameHelper.enableDebugLog(true);
+
+		getWindow().addFlags(WindowManager.LayoutParams.FLAG_KEEP_SCREEN_ON);
+
+		game = new Security();
+
+		AndroidApplicationConfiguration config1 = new AndroidApplicationConfiguration();
+		initialize(game, config);
+
+		gameHelper.setup(this);
+
+	}
+
+	@Override
+	protected void onStart() {
+		super.onStart();
+		gameHelper.onStart(this);
+	}
+
+	@Override
+	protected void onStop() {
+		super.onStop();
+		gameHelper.onStop();
+	}
+
+	@Override
+	protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+		super.onActivityResult(requestCode, resultCode, data);
+
+		// здесь gameHelper принимает решение о подключении, переподключении или
+		// отключении от игровых сервисов, в зависимости от кода результата
+		// Activity
+		gameHelper.onActivityResult(requestCode, resultCode, data);
+
+	}
+
+	@Override
+	public void onSignInFailed() {
+
+	}
+
+	@Override
+	public void onSignInSucceeded() {
+
+	}
+
+	@Override
+	public boolean getSignedInGPGS() {
+		return gameHelper.isSignedIn();;
+	}
+
+	@Override
+	public void loginGPGS() {
+
+		try {
+			runOnUiThread(new Runnable() {
+
+				@Override
+				public void run() {
+					// инициировать вход пользователя. Может быть вызван диалог
+					// входа. Выполняется в UI-потоке
+					gameHelper.beginUserInitiatedSignIn();
+
+				}
+			});
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+
+	}
+
+	@Override
+	public void submitScoreGPGS(int score) {
+
+
+		// отправить игровые очки в конкретную таблицу рекордов с ID
+		// “HgkIr62KmoQJEAIQAQ”
+		Games.Leaderboards.submitScore(gameHelper.getApiClient(), "HgkIr62KmoQJEAIQAQ", score);
+
+	}
+
+	@Override
+	public void unlockAchievementGPGS(String achievementId) {
+
+	}
+
+	@Override
+	public void getLeaderboardGPGS() {
+
+	}
+
+	@Override
+	public void getAchievementsGPGS() {
+
 	}
 }
